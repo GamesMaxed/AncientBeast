bf2d96e1230db7fda1329fdc65775ed88475a0f9
Robin Van den Broeck <robin.broeck@gmail.com>
2018-01-28 15:23:47 +0100

    Added webpack dev server and dashboard

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-env node */
+const path = require('path');
 const merge = require('webpack-merge');
+const Dashboard = require('webpack-dashboard');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 const commonSettings = require('./webpack.common');
+
+const dashboard = new Dashboard();
+
 
 const devSettings = {
   devtool: 'eval-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'deploy'),
+    quiet: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new DashboardPlugin(dashboard.setData),
+  ],
 };
 
 module.exports = merge(commonSettings, devSettings);
